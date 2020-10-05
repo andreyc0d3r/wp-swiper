7c22b1d0e6dd0cfc22471389cf18c5e2a011cfb1
Andrey <andreyc0d3r@gmail.com>
2020-10-06 02:48:52 +1100

    pluginfy

---
+++ src/includes/class-wp-swiper.php
@@ -0,0 +1,142 @@
+<?php
+
+class WP_Swiper {
+    
+    protected $plugin_prefix;
+    protected $plugin_name;
+    protected $version;
+    protected $gb_settings;
+
+	function __construct() {
+        if ( defined( 'DAWPS_PLUGIN_VERSION' ) ) {
+            $this->version = DAWPS_PLUGIN_VERSION;
+        } else {
+            $this->version = '1.0.0';
+        }
+        $this->plguin_prefix = 'daabd';
+        $this->plugin_name = 'adunblocker';
+
+        //$theme = wp_get_theme(); //get the theme object
+        $this->gb_settings = array(
+            'slug'       => 'wpswiper',
+            'title'      => 'WP Swiper Block',
+            'namespace'  => 'wb', //can't contain special characters
+            'category'   => 'common', //`common`, `embed`, `formatting`, `layout`, `widgets`
+            'icon'       => 'admin-users'
+        );
+
+		add_action( 'enqueue_block_editor_assets', array($this, 'register_gutenberg_block') );
+		add_action( 'enqueue_block_editor_assets', array($this, 'enqueue_admin') );
+		add_action( 'wp_enqueue_scripts', array($this, 'enqueue_frontend') );
+	}
+
+    function enqueue_admin() {
+        wp_enqueue_style(
+			$this->gb_settings['slug'].'-block-editor',
+			wb_extension_uri( __FILE__ ) . "/css/admin_block.css",
+			array(),
+			'1.0.0'
+		);
+    }
+
+    function enqueue_frontend() {
+        wp_enqueue_style(
+			$this->gb_settings['slug'].'-block-frontend',
+			wb_extension_uri( __FILE__ ) . "/css/frontend_block.css",
+			array(),
+			'1.0.0'
+		);
+		wp_enqueue_style(
+			'swiper-bundle',
+			"https://unpkg.com/swiper/swiper-bundle.min.css",
+			array(),
+			'1.0.0'
+		);
+        wp_register_script(
+            $this->gb_settings['slug'].'-frontend-js',
+            wb_extension_uri( __FILE__ ) . "/js/frontend_block.js",
+            array('jquery', 'swiperjs'),
+            '1.0.0'
+        );
+        wp_enqueue_script(
+            $this->gb_settings['slug'].'-frontend-js'
+		);
+		wp_enqueue_script(
+			'swiperjs',
+            'https://unpkg.com/swiper/swiper-bundle.min.js'
+		);
+    }
+
+	function register_gutenberg_block(){
+		// Skip block registration if Gutenberg is not enabled/merged.
+		if ( ! function_exists( 'register_block_type' ) ) {
+			return;
+		}
+
+		wp_register_script(
+			$this->gb_settings['slug'].'-block-editor',
+			wb_extension_uri( __FILE__ ) . "/js/admin_block.js",
+			array(
+				'wp-blocks',
+				'wp-i18n',
+				'wp-element',
+				'wp-data',
+				'wp-compose',
+                'wp-components',
+                'wp-api',
+                'wp-api-request',
+                'wp-i18n'
+			),
+			'1.0.0'
+		);
+		wp_localize_script( $this->gb_settings['slug'].'-block-editor', $this->gb_settings['slug'].'_settings', $this->gb_settings );
+
+        wp_enqueue_script($this->gb_settings['slug'].'-block-editor');
+		// register_block_type( $gb_settings['namespace'].'/'.$gb_settings['slug'], array(
+		// 	'editor_script' => $gb_settings['slug'].'-block-editor',
+		// 	'editor_style'  => $gb_settings['slug'].'-block-editor'
+		// ) );
+    }
+    
+    /**
+     * Run the loader to execute all of the hooks with WordPress.
+     *
+     * @since    1.0.0
+     */
+    public function run() {
+        $this->loader->run();
+    }
+
+    /**
+     * The name of the plugin used to uniquely identify it within the context of
+     * WordPress and to define internationalization functionality.
+     *
+     * @since     1.0.0
+     * @return    string    The name of the plugin.
+     */
+    public function get_plugin_name() {
+        return $this->plugin_name;
+    }
+
+    /**
+     * Retrieve the version number of the plugin.
+     *
+     * @since     1.0.0
+     * @return    string    The version number of the plugin.
+     */
+    public function get_version() {
+        return $this->version;
+    }
+
+    /**
+     * Retrieve the prefix of the plugin.
+     *
+     * @since     1.0.0
+     * @return    string    The prefix of the plugin.
+     */
+    public function get_prefix() {
+        return $this->prefix;
+    }
+}
+
+$wp_swiper = new WP_Swiper();
--- src/wp_swiper_block.php
+++ src/wp_swiper_block.php
@@ -1,95 +1,77 @@
 <?php
 
-/*
- * Add an icons block to the Gutenberg editor
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://digitalapps.com
+ * @since             1.0.0
+ * @package           WpSwiper
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Swiper
+ * Plugin URI:        https://digitalapps.com/wp-swiper/
+ * Description:       This plugin detects if Google AdSense ads are blocked by ad-blocker. A popup shows with a text requesting user to whitelist your website.
+ * Version:           1.0.0
+ * Author:            Digital Apps
+ * Author URI:        https://digitalapps.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wpswiper
+ * Domain Path:       /languages
  */
 
-class WP_Swiper{
-
-    private $gb_settings;
-
-	function __construct() {
-        //$theme = wp_get_theme(); //get the theme object
-        $this->gb_settings = array(
-            'slug'       => 'wpswiper',
-            'title'      => 'WP Swiper Block',
-            'namespace'  => 'wb', //can't contain special characters
-            'category'   => 'common', //`common`, `embed`, `formatting`, `layout`, `widgets`
-            'icon'       => 'admin-users'
-        );
-
-		add_action( 'enqueue_block_editor_assets', array($this, 'register_gutenberg_block') );
-		add_action( 'enqueue_block_editor_assets', array($this, 'enqueue_admin') );
-		add_action( 'wp_enqueue_scripts', array($this, 'enqueue_frontend') );
-	}
-
-    function enqueue_admin() {
-        wp_enqueue_style(
-			$this->gb_settings['slug'].'-block-editor',
-			wb_extension_uri( __FILE__ ) . "/css/admin_block.css",
-			array(),
-			'1.0.0'
-		);
-    }
-
-    function enqueue_frontend() {
-        wp_enqueue_style(
-			$this->gb_settings['slug'].'-block-frontend',
-			wb_extension_uri( __FILE__ ) . "/css/frontend_block.css",
-			array(),
-			'1.0.0'
-		);
-		wp_enqueue_style(
-			'swiper-bundle',
-			"https://unpkg.com/swiper/swiper-bundle.min.css",
-			array(),
-			'1.0.0'
-		);
-        wp_register_script(
-            $this->gb_settings['slug'].'-frontend-js',
-            wb_extension_uri( __FILE__ ) . "/js/frontend_block.js",
-            array('jquery', 'swiperjs'),
-            '1.0.0'
-        );
-        wp_enqueue_script(
-            $this->gb_settings['slug'].'-frontend-js'
-		);
-		wp_enqueue_script(
-			'swiperjs',
-            'https://unpkg.com/swiper/swiper-bundle.min.js'
-		);
-    }
-
-	function register_gutenberg_block(){
-		// Skip block registration if Gutenberg is not enabled/merged.
-		if ( ! function_exists( 'register_block_type' ) ) {
-			return;
-		}
-
-		wp_register_script(
-			$this->gb_settings['slug'].'-block-editor',
-			wb_extension_uri( __FILE__ ) . "/js/admin_block.js",
-			array(
-				'wp-blocks',
-				'wp-i18n',
-				'wp-element',
-				'wp-data',
-				'wp-compose',
-                'wp-components',
-                'wp-api',
-                'wp-api-request',
-                'wp-i18n'
-			),
-			'1.0.0'
-		);
-		wp_localize_script( $this->gb_settings['slug'].'-block-editor', $this->gb_settings['slug'].'_settings', $this->gb_settings );
-
-        wp_enqueue_script($this->gb_settings['slug'].'-block-editor');
-		// register_block_type( $gb_settings['namespace'].'/'.$gb_settings['slug'], array(
-		// 	'editor_script' => $gb_settings['slug'].'-block-editor',
-		// 	'editor_style'  => $gb_settings['slug'].'-block-editor'
-		// ) );
-	}
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
 }
 
-$wp_swiper = new WP_Swiper();
+define( 'DAWPS_PLUGIN_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-adunblocker-activator.php
+ */
+function activate_adunblocker() {
+    require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-swiper-activator.php';
+    AdUnblocker_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-adunblocker-deactivator.php
+ */
+function deactivate_adunblocker() {
+    require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-swiper-deactivator.php';
+    AdUnblocker_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_wpswiper' );
+register_deactivation_hook( __FILE__, 'deactivate_wpswiper' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-adunblocker.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_wpswiper() {
+
+    $plugin = new WP_Swiper();
+    $plugin->run();
+
+}
+run_wpswiper();
