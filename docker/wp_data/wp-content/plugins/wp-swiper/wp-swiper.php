a6344b50cd40293c5a51d227d31a928f566acf39
Andrey <andreyc0d3r@gmail.com>
2023-04-23 14:01:11 +1000

    version bump 1.3.0

--- docker/wp_data/wp-content/plugins/wp-swiper/wp-swiper.php
+++ docker/wp_data/wp-content/plugins/wp-swiper/wp-swiper.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WP Swiper
  * Plugin URI:        https://digitalapps.com/wp-swiper/
  * Description:       Swiper JS as a Gutenberg Block.
- * Version:           1.0.29
+ * Version:           1.0.30
  * Author:            Digital Apps
  * Author URI:        https://digitalapps.com/
  * License:           GPL-2.0+
