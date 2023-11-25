dc296f05cc8e36d3e6ed6bccb39ef13f44064b29
Andrey <andreyc0d3r@gmail.com>
2023-11-25 16:19:35 +1100

    formatting

--- src/gutenberg/js/admin_block.dev.js
+++ src/gutenberg/js/admin_block.dev.js
@@ -3,20 +3,17 @@ import '../../css/editor.scss';
 import * as slides from './slides';
 import * as slide from './slide';
 
-const {
-    registerBlockType,
-    registerBlockStyle
-} = wp.blocks;
+const { registerBlockType, registerBlockStyle } = wp.blocks;
 
 /**
  * Register blocks
  */
-registerBlockType( slides.name, slides.settings );
-registerBlockType( slide.name, slide.settings );
+registerBlockType(slides.name, slides.settings);
+registerBlockType(slide.name, slide.settings);
 
-registerBlockStyle( slides.name, [
-    {
-        name: 'testimonials',
-        label: 'Testimonials'
-    }
-] );
+registerBlockStyle(slides.name, [
+	{
+		name: 'testimonials',
+		label: 'Testimonials',
+	},
+]);
